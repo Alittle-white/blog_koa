6bc8429c5014102203c24219bc7bad527ed2ef82
Alittle-white <1037067196@qq.com>
2022-04-22 17:50:48 +0800

    add

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ app.use(
           // if (ctx.url === '/test') {
           //     return '*'; // 允许来自所有域名请求
           // }
-          // return 'http://172.21.40.112:8080'; //只允许http://localhost:8080这个域名的请求
-          return '*'; // 允许来自所有域名请求
+          return 'http://172.21.40.112:8080'; //只允许http://localhost:8080这个域名的请求
+          // return '*'; // 允许来自所有域名请求
       },
       maxAge: 5, //指定本次预检请求的有效期，单位为秒。
-      // credentials: true, //是否允许发送Cookie
-      credentials: false, //是否允许发送Cookie
+      credentials: true, //是否允许发送Cookie
+      // credentials: false, //是否允许发送Cookie
       allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法'
       allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
       exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
